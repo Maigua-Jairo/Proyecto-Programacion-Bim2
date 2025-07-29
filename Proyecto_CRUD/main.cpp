2036176785e0b6daf8f6dcefa76e0e461d086ab4
anthonyle1108 <anthony.ledesma@epn.edu.ec>
2025-07-28 20:32:50 -0500

    Verificacion del codigo main.cpp

--- Proyecto_CRUD/main.cpp
+++ Proyecto_CRUD/main.cpp
@@ -5,8 +5,9 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setStyle("fusion");
+    a.setStyle("fusion"); 
     MainWindow w;
     w.show();
     return a.exec();
 }
+//Codigo vrefificado
