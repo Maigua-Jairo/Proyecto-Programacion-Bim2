84dadb7338e1aa803d10ffa17766949be91d534e
diegod25122 <diego.camacho@epn.edu.ec>
2025-07-28 20:36:20 -0500

    Update main.cpp
    modificado el main ya que se modifico el eliminar.cpp

--- Proyecto_CRUD/main.cpp
+++ Proyecto_CRUD/main.cpp
@@ -1,13 +1,14 @@
 #include "mainwindow.h"
+
 #include <QApplication>
 using namespace std;
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    a.setStyle("fusion"); 
+    a.setStyle("fusion");
     MainWindow w;
     w.show();
     return a.exec();
 }
+
 //Codigo vrefificado
